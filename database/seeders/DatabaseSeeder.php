794dde92fe51b90267ab59933a8d9f32381e0558
amr-zaky <amr.zaky367@yahoo.com>
2021-06-27 06:09:19 +0200

    admin factory updated'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(5)->create();
-         \App\Models\Client::factory(20)->create();
+
+         // \App\Models\User::factory(1)->create();    //to create email:admin@admin.com password:password
+        // \App\Models\Client::factory(20)->create();
        //  \App\Models\ClientInvoice::factory(50)->create();
     }
 }
